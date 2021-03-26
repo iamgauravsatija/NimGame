bd70494536cc22a3d228d8f106d75f4eb90e974c
JoshuaMuir2 <36827006+JoshuaMuir2@users.noreply.github.com>
2021-03-26 15:35:17 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 def main():
     k = int(input("Define K "))
@@ -9,13 +10,14 @@ def main():
     piles.append(1)
     piles.append(2)
     piles.append(3)
-    # piles.append(1)
-    # piles.append(2)
-    # piles.append(3)
+    piles.append(1)
+    piles.append(2)
+    piles.append(3)
     piles.append(0)
     piles.append(0)
     #temporary check win for debugging
-    #check_win(k,piles)
+    #check_win_con1("P1",k,piles)
+    #check_win_con4("P1",k,piles)
     #using 3 piles with an arbitrary number of objects for now
     turnOrder = choose_start()
     while True:
@@ -23,16 +25,29 @@ def main():
             display_pile(piles)
             toRemove = player_turn()
             piles = remove_object(piles,toRemove[0],toRemove[1])
-            check_win("P1",k,piles)
+            check_win_con1("P1",k,piles)
+            check_win_con2("P1", k, piles)
+            check_win_con3("P1", k, piles)
+            check_win_con4("P1", k, piles)
+
             enemy_turn(piles)
-            check_win("P2",k,piles)
+            check_win_con1("P2",k,piles)
+            check_win_con2("P2", k, piles)
+            check_win_con3("P2", k, piles)
+            check_win_con4("P2", k, piles)
         else:
             enemy_turn(piles)
-            check_win("P2",k,piles)
+            check_win_con1("P2",k,piles)
+            check_win_con2("P2", k, piles)
+            check_win_con3("P2", k, piles)
+            check_win_con4("P2", k, piles)
             display_pile(piles)
             toRemove = player_turn()
             piles = remove_object(piles,toRemove[0],toRemove[1])
-            check_win("P1",k,piles)
+            check_win_con1("P1",k,piles)
+            check_win_con2("P1", k, piles)
+            check_win_con3("P1", k, piles)
+            check_win_con4("P1", k, piles)
 
 def display_pile(piles):
     print("Displaying the Pile")
@@ -65,7 +80,7 @@ def player_turn():
     return returnList
 
 
-def check_win(playerName,k,objectPiles):
+def check_win_con1(playerName,k,objectPiles):
     #TODO put each section in separate method :)
 
     allEmpty = True
@@ -75,6 +90,9 @@ def check_win(playerName,k,objectPiles):
             allEmpty = False
     if(allEmpty):
         print(playerName + " lost the game")
+        sys.exit()
+
+def check_win_con2(playerName,k,objectPiles):
     #the second section is for the condition "3 piles each with 2 objects and all other piles are empty"
     emptyPilesCounter = 0
     emptyPilesLimit = len(objectPiles)-3
@@ -87,6 +105,9 @@ def check_win(playerName,k,objectPiles):
             pilesTwoObjCounter+=1
     if(emptyPilesCounter == emptyPilesLimit and pilesTwoObjLimit == pilesTwoObjCounter):
         print(playerName + " lost the game")
+        sys.exit()
+
+def check_win_con3(playerName, k, objectPiles):
     #the third section is for the condition "1 pile with 1 object, 1 pile with 2 objects, 1 pile with 3 objects, all other piles are empty"
     oneObjectPileCounter = 0
     twoObjectPileCounter = 0
@@ -105,6 +126,9 @@ def check_win(playerName,k,objectPiles):
 
     if oneObjectPileCounter ==1 and twoObjectPileCounter==1 and threeObjectPileCounter==1 and zeroObjectPileCounter==zeroObjectPileLimit:
         print(playerName + " lost the game")
+        sys.exit()
+
+def check_win_con4(playerName,k,objectPiles):
     #the fourth section os for the condition "2 piles each with 1 object, 2 piles each with 2 objects, the others are empty, this is the general case and has been tested for k=2 and k=3"
     kPilesValue = []
     kPilesCounter = []
@@ -128,6 +152,7 @@ def check_win(playerName,k,objectPiles):
 
     if(zeroObjectPileLimit == zeroObjectPileCounter and currentBool):
         print(playerName + " lost the game")
+        sys.exit()
 
 
 main()
